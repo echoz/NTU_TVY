8f26a2b7cfd8ac75e8246f53d07e3eaafc7cb57f
echoz <jeremyfoo@gmail.com>
2011-08-11 21:33:03 +0800

    added uikit framework and delegate

---
+++ Classes/NTU_TVY_TraversityDelegate.h
@@ -0,0 +1,17 @@
+//
+//  NTU_TVY_TraversityDelegate.h
+//  NTU_TVY
+//
+//  Created by Jeremy Foo on 8/11/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "TCApplicationDelegate.h"
+
+@interface NTU_TVY_TraversityDelegate : TCApplicationDelegate {
+	UINavigationController *nav;
+}
+
+@property (readonly) UINavigationController *nav;
+
+@end
---
+++ Classes/NTU_TVY_TraversityDelegate.m
@@ -0,0 +1,15 @@
+//
+//  NTU_TVY_TraversityDelegate.m
+//  NTU_TVY
+//
+//  Created by Jeremy Foo on 8/11/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "NTU_TVY_TraversityDelegate.h"
+
+@implementation NTU_TVY_TraversityDelegate
+@synthesize nav;
+
+
+@end
