51e76ebb1a8697da534813fd29acf157efdfa2b9
echoz <jeremyfoo@gmail.com>
2011-08-11 21:51:18 +0800

    tableview of stops

---
+++ UI/StopsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  StopsTableViewController.h
+//  NTU_TVY
+//
+//  Created by Jeremy Foo on 8/11/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StopsTableViewController : UITableViewController
+
+@end
